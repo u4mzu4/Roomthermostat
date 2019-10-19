46c6c74833af5ca00f198f4bc69dcb389b8f2010
andrew911 <39950568+u4mzu4@users.noreply.github.com>
2019-10-19 19:23:10 +0200

    WebServer first try

--- Thermostat_v2.0_limited.ino
+++ Thermostat_v2.0_limited.ino
@@ -16,6 +16,8 @@
 #include <i2cEncoderLibV2.h>
 #include <icons.h>
 #include <OpenTherm.h>
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
 
 //Enum
 enum DISPLAY_SM {
@@ -111,6 +113,7 @@ DallasTemperature sensor(&oneWire);
 DeviceAddress sensorDeviceAddress;
 i2cEncoderLibV2 Encoder(ENCODER_ADDRESS);
 OpenTherm ot(OTPIN_IN, OTPIN_OUT);
+AsyncWebServer server(80);
 
 void GetWaterTemp()
 {
@@ -945,6 +948,143 @@ void ErrorManager(ERROR_T errorID, int errorCounter, int errorLimit)
   terminal.flush();
 }
 
+String processor(const String& var)
+{
+  String tempvar = var;
+  tempvar.remove(2);
+  int numberofPH = tempvar.toInt();
+  switch (numberofPH) {
+    case 1:
+      return (String(actualHumidity, 0) + " %%");
+      break;
+    case 2:
+      return (String(actualPressure, 0) + " mbar");
+      break;
+    case 3:
+      return (String(actualTemperature, 1) + " °C");
+      break;
+    case 4:
+      if (radiatorON)
+      {
+        return String("on");
+      }
+      else
+      {
+        return String("off");
+      }
+      break;
+    case 5:
+      if (boilerON)
+      {
+        return String("on");
+      }
+      else
+      {
+        return String("off");
+      }
+      break;
+    case 6:
+      if (floorON)
+      {
+        return String("on");
+      }
+      else
+      {
+        return String("off");
+      }
+      break;
+    case 7:
+      return (String(bmeTemperature, 1) + " °C");
+      break;
+    case 8:
+      return (String(transData[1], 1) + " °C");
+      break;
+    case 9:
+      return (String(kitchenTemp, 1) + " °C");
+      break;
+    case 10:
+      return (String(waterTemperature, 1) + " °C");
+      break;
+    case 11:
+      return (String(setValue, 1));
+      break;
+    case 12:
+      return (String(setFloorTemp, 1));
+      break;
+    case 13:
+      if (setControlBase == 1)
+      {
+        return String("checked");
+      }
+    case 14:
+      if (setControlBase == 2)
+      {
+        return String("checked");
+      }
+    case 15:
+      if (heatingON)
+      {
+        return String("checked");
+      }
+    default:
+      return String();
+      break;
+  }
+  return String();
+}
+
+void SetupWebServer ()
+{
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    int paramsNr = request->params();
+    if (4 == paramsNr)
+    {
+      for (int j = 0; j < paramsNr; j++)
+      {
+        AsyncWebParameter* p = request->getParam(j);
+        if (String("p1") == p->name())
+        {
+          setValue = p->value().toFloat();
+          //Blynk.virtualWrite(V5, setValue);
+        }
+        if (String("p2") == p->name())
+        {
+          heatingON = (bool)(p->value().toInt());
+          //Blynk.virtualWrite(V2, heatingON);
+        }
+        if (String("p3") == p->name())
+        {
+          setFloorTemp  = p->value().toFloat();
+          //Blynk.virtualWrite(V6, setFloorTemp);
+        }
+        if (String("p4") == p->name())
+        {
+          setControlBase  = p->value().toInt();
+          //Blynk.virtualWrite(V12, setControlBase);
+        }
+      }
+    }
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  // Route to load circle.css file
+  server.on("/circle.css", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/circle.css", "text/css");
+  });
+
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/script.js", "text/javascript");
+  });
+  server.begin();
+}
+
+
+
 void setup() {
   pinMode(RELAYPIN1, OUTPUT);
   pinMode(RELAYPIN2, OUTPUT);
@@ -1000,7 +1140,7 @@ void setup() {
   Blynk.virtualWrite(V9, radiatorON);
 
   ot.begin(handleInterrupt);
-
+  SetupWebServer();
   MainTask();
 }
 
